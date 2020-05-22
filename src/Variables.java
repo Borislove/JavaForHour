0849f8d0730e5b1fb14b3d59aef07f79ab249d0e
Borislove <abirme@yandex.ru>
2020-05-22 16:47:19 +0300

    переменные

---
+++ src/Variables.java
@@ -0,0 +1,25 @@
+//переменные
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte num = 2;
+        int i = 234567;
+        long l = 12345678924235234L;
+
+        float num_2 = 324.234f;
+        double num_3;
+        num_3 = 23543.2345;
+
+        char sym = 'A';
+        System.out.println(sym);
+
+        boolean is_happy = true;
+        System.out.println(is_happy);
+
+        String str = "Hello World!";
+        System.out.println(str);
+
+        System.out.println("Variable = " + num);
+    }
+}
