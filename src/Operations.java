4e5f0bf2cb0894ac07610325684d893ed250d8b1
Borislove <abirme@yandex.ru>
2020-05-22 16:55:17 +0300

    математические операции

---
+++ src/Operations.java
@@ -0,0 +1,11 @@
+//математические операции
+public class Operations {
+    public static void main(String[] args) {
+
+        float num_1 = 23.67f, num_2 = 43.4f, res;
+        res = num_1 + num_2;
+        res += 45f;
+        res ++;
+        System.out.println(res);
+    }
+}
