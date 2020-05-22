29af8f597b9ebc951cc900d84c9f79306216c277
Borislove <abirme@yandex.ru>
2020-05-22 17:21:39 +0300

    циклы

---
+++ src/Cycle.java
@@ -0,0 +1,24 @@
+public class Cycle {
+    public static void main(String[] args) {
+
+        //в любом случае 1 раз зайдем в цикл
+        int i = 100;
+        do {
+            System.out.println("Element i is: " + i);
+
+        } while (i < 10);
+
+//        int i = 0;
+//        while (i <= 15) {
+//            System.out.println("Element i is: " + i);
+//            i++;
+//        }
+
+    /*    for (int i = 100; i >= 10; i -= 10) {
+            System.out.println("Element i is: " + i);
+        }*/
+    }
+}
+
+
+//фигурные скобки можно опустить если 1 строка кода в цикле
